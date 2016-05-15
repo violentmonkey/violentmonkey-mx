a0196d3d4bdd3b4514fdf9331bb07f534a07542e
Gerald <gera2ld@163.com>
2016-05-15 11:18:48 +0800

    feat: modularization

--- src/options/views/edit.js
+++ src/options/views/edit.js
@@ -1,103 +1,110 @@
-var EditView = BaseView.extend({
-  className: 'frame edit',
-  templateUrl: '/options/templates/edit.html',
-  events: {
-    'click .button-toggle': 'toggleButton',
-    'change [data-id]': 'updateCheckbox',
-    'click #editorSave': 'save',
-    'click #editorClose': 'close',
-    'click #editorSaveClose': 'saveClose',
-  },
-  initialize: function () {
-    var _this = this;
-    _.bindAll(_this, 'save', 'close');
-    BaseView.prototype.initialize.call(_this);
-    _this.metaModel = new Meta(_this.model.toJSON(), {parse: true});
-    _this.listenTo(_this.metaModel, 'change', function (model) {
-      _this.model.set('custom', model.toJSON());
-    });
-    _this.listenTo(_this.model, 'change', function (model) {
-      _this.updateStatus(true);
-    });
-  },
-  _render: function () {
-    var _this = this;
-    var it = _this.model.toJSON();
-    _this.$el.html(_this.templateFn(it));
-    var gotScript = it.id ? _.sendMessage({
-      cmd: 'GetScript',
-      data: it.id,
-    }) : Promise.resolve(it);
-    _this.loadedEditor = _.initEditor({
-      container: _this.$('.editor-code')[0],
-      onsave: _this.save,
-      onexit: _this.close,
-      onchange: function (e) {
-        _this.model.set('code', _this.editor.getValue());
-      },
-    });
-    Promise.all([
-      gotScript,
-      _this.loadedEditor,
-    ]).then(function (res) {
-      var script = res[0];
-      var editor = _this.editor = res[1];
-      editor.setValueAndFocus(script.code);
-      editor.clearHistory();
-      _this.updateStatus(false);
-    });
-  },
-  updateStatus: function (changed) {
-    this.changed = changed;
-    this.$('#editorSave').prop('disabled', !changed);
-    this.$('#editorSaveClose').prop('disabled', !changed);
-  },
-  save: function () {
-    var _this = this;
-    var data = _this.model.toJSON();
-    return _.sendMessage({
-      cmd: 'ParseScript',
-      data: {
-        id: data.id,
-        code: data.code,
-        // User created scripts MUST be marked `isNew` so that
-        // the backend is able to check namespace conflicts
-        isNew: !data.id,
-        message: '',
-        more: {
-          custom: data.custom,
-          update: data.update,
-        }
-      }
-    }).then(function (script) {
-      _this.model.set('id', script.id);
-      _this.updateStatus(false);
-    }, function (err) {
-      _.showMessage({
-        data: err,
+define('views/Edit', function (require, _exports, module) {
+  var BaseView = require('cache').BaseView;
+  var MetaView = require('views/Meta');
+  var models = require('models');
+  var app = require('app');
+
+  module.exports = BaseView.extend({
+    className: 'frame edit',
+    templateUrl: '/options/templates/edit.html',
+    events: {
+      'click .button-toggle': 'toggleButton',
+      'change [data-id]': 'updateCheckbox',
+      'click #editorSave': 'save',
+      'click #editorClose': 'close',
+      'click #editorSaveClose': 'saveClose',
+    },
+    initialize: function () {
+      var _this = this;
+      _.bindAll(_this, 'save', 'close');
+      BaseView.prototype.initialize.call(_this);
+      _this.metaModel = new models.Meta(_this.model.toJSON(), {parse: true});
+      _this.listenTo(_this.metaModel, 'change', function (model) {
+        _this.model.set('custom', model.toJSON());
       });
-    });
-  },
-  close: function () {
-    if (!this.changed || confirm(_.i18n('confirmNotSaved')))
-      scriptList.trigger('edit:close');
-  },
-  saveClose: function () {
-    this.save().then(this.close);
-  },
-  toggleButton: function (e) {
-    if (this.metaView) {
-      this.$(e.target).removeClass('active');
-      this.metaView.remove();
-      this.metaView = null;
-    } else {
-      this.$(e.target).addClass('active');
-      this.metaView = new MetaView({model: this.metaModel});
-      this.metaView.$el.insertAfter(e.target);
-    }
-  },
-  updateCheckbox: function (e) {
-    var res = this.getValue(e.target);
-    this.model.set(res.key, res.value);
-  },
+      _this.listenTo(_this.model, 'change', function (_model) {
+        _this.updateStatus(true);
+      });
+    },
+    _render: function () {
+      var _this = this;
+      var it = _this.model.toJSON();
+      _this.$el.html(_this.templateFn(it));
+      var gotScript = it.id ? _.sendMessage({
+        cmd: 'GetScript',
+        data: it.id,
+      }) : Promise.resolve(it);
+      _this.loadedEditor = _.initEditor({
+        container: _this.$('.editor-code')[0],
+        onsave: _this.save,
+        onexit: _this.close,
+        onchange: function (_e) {
+          _this.model.set('code', _this.editor.getValue());
+        },
+      });
+      Promise.all([
+        gotScript,
+        _this.loadedEditor,
+      ]).then(function (res) {
+        var script = res[0];
+        var editor = _this.editor = res[1];
+        editor.setValueAndFocus(script.code);
+        editor.clearHistory();
+        _this.updateStatus(false);
+      });
+    },
+    updateStatus: function (changed) {
+      this.changed = changed;
+      this.$('#editorSave').prop('disabled', !changed);
+      this.$('#editorSaveClose').prop('disabled', !changed);
+    },
+    save: function () {
+      var _this = this;
+      var data = _this.model.toJSON();
+      return _.sendMessage({
+        cmd: 'ParseScript',
+        data: {
+          id: data.id,
+          code: data.code,
+          // User created scripts MUST be marked `isNew` so that
+          // the backend is able to check namespace conflicts
+          isNew: !data.id,
+          message: '',
+          more: {
+            custom: data.custom,
+            update: data.update,
+          }
+        }
+      }).then(function (script) {
+        _this.model.set('id', script.id);
+        _this.updateStatus(false);
+      }, function (err) {
+        _.showMessage({
+          data: err,
+        });
+      });
+    },
+    close: function () {
+      if (!this.changed || confirm(_.i18n('confirmNotSaved')))
+      app.scriptList.trigger('edit:close');
+    },
+    saveClose: function () {
+      this.save().then(this.close);
+    },
+    toggleButton: function (e) {
+      if (this.metaView) {
+        this.$(e.target).removeClass('active');
+        this.metaView.remove();
+        this.metaView = null;
+      } else {
+        this.$(e.target).addClass('active');
+        this.metaView = new MetaView({model: this.metaModel});
+        this.metaView.$el.insertAfter(e.target);
+      }
+    },
+    updateCheckbox: function (e) {
+      var res = this.getValue(e.target);
+      this.model.set(res.key, res.value);
+    },
+  });
 });
--- src/popup/views/item.js
+++ src/popup/views/item.js
@@ -1,28 +1,32 @@
-var MenuItemView = BaseView.extend({
-  className: 'menu-item',
-  templateUrl: '/popup/templates/menuitem.html',
-  events: {
-    'click': 'onClick',
-  },
-  initialize: function () {
-    BaseView.prototype.initialize.call(this);
-    this.listenTo(this.model, 'change', this.render);
-    this.listenTo(this.model, 'remove', this.onRemove);
-  },
-  _render: function () {
-    var it = this.model.toJSON();
-    if (typeof it.symbol === 'function')
+define('views/MenuItem', function (require, _exports, module) {
+  var BaseView = require('cache').BaseView;
+
+  module.exports = BaseView.extend({
+    className: 'menu-item',
+    templateUrl: '/popup/templates/menuitem.html',
+    events: {
+      'click': 'onClick',
+    },
+    initialize: function () {
+      BaseView.prototype.initialize.call(this);
+      this.listenTo(this.model, 'change', this.render);
+      this.listenTo(this.model, 'remove', this.onRemove);
+    },
+    _render: function () {
+      var it = this.model.toJSON();
+      if (typeof it.symbol === 'function')
       it.symbol = it.symbol(it.data);
-    this.$el.html(this.templateFn(it))
-    .attr('title', it.title === true ? it.name : it.title);
-    if (it.data === false) this.$el.addClass('disabled');
-    else this.$el.removeClass('disabled');
-  },
-  onClick: function (e) {
-    var onClick = this.model.get('onClick');
-    onClick && onClick(e, this.model);
-  },
-  onRemove: function () {
-    this.$el.remove();
-  },
-})
+      this.$el.html(this.templateFn(it))
+      .attr('title', it.title === true ? it.name : it.title);
+      if (it.data === false) this.$el.addClass('disabled');
+      else this.$el.removeClass('disabled');
+    },
+    onClick: function (e) {
+      var onClick = this.model.get('onClick');
+      onClick && onClick(e, this.model);
+    },
+    onRemove: function () {
+      this.$el.remove();
+    },
+  });
+});
