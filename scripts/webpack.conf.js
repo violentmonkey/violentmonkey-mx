a0b55ec42f7ccb67f21aa79672873c192e9cd3e4
Gerald <gera2ld@163.com>
2017-06-04 00:23:53 +0800

    chore: decrease package size

--- scripts/webpack.conf.js
+++ scripts/webpack.conf.js
@@ -5,38 +5,46 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const base = require('./webpack.base.conf');
 const { IS_DEV } = require('./utils');
 
+const entry = {
+  'background/app': 'src/background/app.js',
+  'options/app': 'src/options/app.js',
+  'popup/app': 'src/popup/app.js',
+  injected: 'src/injected/index.js',
+};
+
 module.exports = Object.assign({}, base, {
-  entry: {
-    'background/app': 'src/background/app.js',
-    'options/app': 'src/options/app.js',
-    'popup/app': 'src/popup/app.js',
-    injected: 'src/injected/index.js',
-  },
+  entry,
   plugins: [
     ... base.plugins,
-    // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
       name: 'browser',
+      chunks: Object.keys(entry),
+      minChunks: 4,
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      chunks: Object.keys(entry).filter(key => key !== 'injected'),
+      minChunks: 2,
     }),
     new HtmlWebpackPlugin({
       filename: 'background/index.html',
       template: 'src/background/index.html',
       inject: true,
-      chunks: ['browser', 'background/app'],
+      chunks: ['vendor', 'browser', 'background/app'],
       chunksSortMode: 'dependency'
     }),
     new HtmlWebpackPlugin({
       filename: 'options/index.html',
       template: 'src/options/index.html',
       inject: true,
-      chunks: ['browser', 'options/app'],
+      chunks: ['vendor', 'browser', 'options/app'],
       chunksSortMode: 'dependency'
     }),
     new HtmlWebpackPlugin({
       filename: 'popup/index.html',
       template: 'src/popup/index.html',
       inject: true,
-      chunks: ['browser', 'popup/app'],
+      chunks: ['vendor', 'browser', 'popup/app'],
       chunksSortMode: 'dependency'
     }),
     // new FriendlyErrorsPlugin(),
